00b4286d75704d867304f0c2af0b1f5d67ef49c7
Tomm-debug <tobergallo@gmail.com>
2025-02-17 01:19:05 -0300

    Add files via upload

---
+++ Manipulacion_de_cadenas/ConvertirMayuscula.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main() {
+    char letra = 'a'; // Declaración de un carácter
+    char mayuscula = toupper(letra); // Convertir carácter a mayúscula usando función toupper() de biblioteca cctype
+    cout << "La letra en mayúscula es: " << mayuscula << endl; // Imprimir el carácter en mayúscula
+    int abc ;
+    cin >> abc ;
+    return 0;
+}
---
+++ Operaciones_con_cadenas/Subcadena.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string cadena = "Programacion en C++"; // Declaración de una cadena de caracteres utilizando la clase string
+    string subcadena = cadena.substr(13, 3); // Obtener subcadena "C++" utilizando el método substr() de clase string
+    cout << "La subcadena es: " << subcadena << endl; // Imprimir la subcadena
+    int abc ;
+    cin >> abc ;
+    return 0;
+}
